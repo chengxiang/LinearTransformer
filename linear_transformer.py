d20bf25f060a8ef1722b7eb580258ffdaee56f86
Xiang Cheng <chengx@suvrit-gpus.lids.mit.edu>
2023-10-01 15:28:18 -0400

    file containing main model definition

---
+++ linear_transformer.py
@@ -0,0 +1,142 @@
+###########################################
+# This file contains the following:
+# 1. Linear Transformer Model
+# 2. Function for clipping gradient
+# 3. Function for generating random data
+#
+# The notation for linear attention follows
+# the paper at https://arxiv.org/pdf/2306.00297.pdf
+###########################################
+
+
+import torch
+from torch import nn
+import numpy as np
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Definition of a single linear attention unit for linear-regression data
+# P is the value matrix
+# Q is the product of key,query matrices
+# the dimensions of the input are
+# B: batch-size of prompts
+# N: context length (excluding query)
+# d: covariate dimension
+# P,Q are d x d matrices
+# Z is a B x (N+1) + (d+1) matrix
+# Output is also B x (N+1) + (d+1)
+
+# For linear attention, activation = None
+# For standard attention, activation(x) = torch.nn.functional.softmax(x, dim = 2)
+# For ReLU attention, activation(x) = torch.nn.relu(x)
+def attention(P,Q,Z, activation = None):
+    B= Z.shape[0]
+    N = Z.shape[1]-1
+    d = Z.shape[2]-1
+    P_full =  torch.cat([P,torch.zeros(1,d).to(device)],dim=0)
+    P_full =  torch.cat([P_full,torch.zeros(d+1,1).to(device)],dim=1)
+    P_full[d,d] = 1
+    Q_full = torch.cat([Q, torch.zeros(1,d).to(device)],dim=0)
+    Q_full = torch.cat([Q_full, torch.zeros(d+1,1).to(device)],dim=1)
+    A = torch.eye(N+1).to(device)
+    A[N,N] = 0
+    Attn = torch.einsum('BNi, ij, BMj -> BNM', (Z,Q_full,Z))
+    if activation is not None:
+        Attn = activation(Attn)
+    key = torch.einsum('ij, BNj -> BNi', (P_full,Z))
+    Output = torch.einsum('BNM,ML, BLi -> BNi', (Attn,A,key))
+    return Output /N
+
+
+# The Linear Transformer module
+# n_layer denotes the number of layers
+# n_head denotes the number of heads. In most of our experiments, n_head = 1
+# d denotes the dimension of covariates
+# var denotes the variance of initialization. It needs to be sufficiently small, but exact value is not important
+# allparam: contains all the parameters, has dimension n_layer x n_head x 2 x d x d
+# For example
+# - P matrix at layer i, head j is allparam[i,j,0,:,:]
+# - Q matrix at layer i, head j is allparam[i,j,1,:,:]
+class Transformer_F(nn.Module):
+    def __init__(self, n_layer, n_head, d, var):
+        super(Transformer_F, self).__init__()
+        self.register_parameter('allparam', torch.nn.Parameter(torch.zeros(n_layer, n_head, 2, d, d)))
+        with torch.no_grad():
+            self.allparam.normal_(0,var)
+        self.n_layer = n_layer
+        self.n_head = n_head
+
+    def forward(self, Z):
+        for i in range(self.n_layer):
+            Zi = Z
+            residues = 0
+            # the forwarad map of each layer is given by F(Z) = Z + attention(Z)
+            for j in range(self.n_head):
+                Pij = self.allparam[i,j,0,:,:]
+                Qij = self.allparam[i,j,1,:,:]
+                residues = residues + attention(Pij,Qij,Zi)
+            Z = Zi + residues
+        return Z
+
+# evaluate the loss of model, given data (Z,y)
+def in_context_loss(model, Z, y):
+    N = Z.shape[1]-1
+    d = Z.shape[2]-1
+    output = model(Z)
+    diff = output[:,N,d]+y
+    loss = ((diff)**2).mean() 
+    return loss
+        
+# generate random data for linear regression
+# mode: distribution of samples to generate. Currently supports 'normal', 'gamma', 'sphere'
+# N: number of context examples
+# d: dimension of covariates
+# For gamma distribution:
+# - shape_k: shape parameter of gamma distribution (unused otherwise)
+# - scale parameter: hard coded so that when shape_k = 5/2 and d=5, the generated data is standard normal
+def generate_data(mode='normal',N=20,d=1,B=1000,shape_k=0.1, U=None, D=None):
+    W= torch.FloatTensor(B, d).normal_(0,1).cuda()
+    X = torch.FloatTensor(B, N, d).normal_(0, 1).cuda()
+    X_test = torch.FloatTensor(B,1,d).normal_(0, 1).cuda()
+    
+    if U is not None:
+        U = U.cuda()
+        D = D.cuda()
+        W= torch.FloatTensor(B, d).normal_(0,1).cuda()
+        W = torch.mm(W,torch.inverse(D))
+        W = torch.mm(W,U.t())
+    
+    if mode =='sphere':
+        X.div_(X.norm(p=2,dim=2)[:,:,None])
+        X_test.div_(X_test.norm(p=2,dim=2)[:,:,None])
+    elif mode == 'gamma':
+        # random gamma scaling for X
+        gamma_scales = np.random.gamma(shape=shape_k, scale=(10/shape_k)**(0.5), size=[B,N])
+        gamma_scales = torch.Tensor(gamma_scales).cuda()
+        gamma_scales = gamma_scales.sqrt()
+        # random gamma scaling for X_test
+        gamma_test_scales = np.random.gamma(shape=shape_k, scale=(10/shape_k)**(0.5), size=[B,1])
+        gamma_test_scales = torch.Tensor(gamma_test_scales).cuda()
+        gamma_test_scales = gamma_test_scales.sqrt()
+        # normalize to unit norm
+        X.div_(X.norm(p=2,dim=2)[:,:,None])
+        X_test.div_(X_test.norm(p=2,dim=2)[:,:,None])
+        # scale by gamma
+        X.mul_(gamma_scales[:,:,None])
+        X_test.mul_(gamma_test_scales[:,:,None])
+    elif mode =='normal':
+        assert True
+    else:
+        assert False
+        
+    if U is not None:
+        X = torch.einsum('ij, jk, BNk -> BNi', (U,D,X))
+        X_test = torch.einsum('ij, jk, BNk -> BNi', (U,D,X_test))
+        
+    y = torch.einsum('bi,bni->bn', (W, X)).unsqueeze(2)
+    y_zero = torch.zeros(B,1,1).cuda()
+    y_test = torch.einsum('bi,bni->bn', (W, X_test)).squeeze(1)
+    X_comb= torch.cat([X,X_test],dim=1)
+    y_comb= torch.cat([y,y_zero],dim=1)
+    Z= torch.cat([X_comb,y_comb],dim=2)
+    return Z.to(device),y_test.to(device)
